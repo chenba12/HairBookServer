8af37f7a677bbb813807d05655be9bb63e81ce91
Chen Ben Ami <chen24201@gmail.com>
2024-01-09 19:21:24 +0200

    users path

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,38 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+// nothing
+router.get('/allbarbershops', (req, res)=> {
+
 });
+// barbershop id
+router.get('/showreviews',(req,res)=>{
+
+});
+// barbershop id, full name, date, review
+router.post('/postreview',(req,res)=>{
+});
+// barbershop id, full name, date, review
+router.put('/updatereview',(req,res)=>{
+});
+// barbershop id, full name, date, review
+router.delete('/deletereview',(req,res)=>{
+});
+// full name, id, barbershop id, date
+router.post(('/bookhaircut'),(req,res)=>{
+
+});
+// barbershop id, book id, date
+router.put(('/updatehaircut'),(req,res)=>{
+
+});
+// book id
+router.delete(('/deletehaircut'),(req,res)=>{
+
+});
+// id, full name, age
+router.put('/updatemydetails',(req,res)=>{
+
+})
 
 module.exports = router;
