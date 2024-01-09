5b72f6680f444bd02cfceb45a7d8eb9947dcf31c
Benit1 <110784868+Benit1@users.noreply.github.com>
2024-01-09 17:55:30 +0200

    middleware client to server done

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-console.log("benit")
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var app = express();
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const fs = require('fs');
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const app = express();
+
+// Custom Logging Middleware using Morgan
+
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -18,16 +23,52 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+const  customLogger = (req, res, next)=>  {
+  // Get request details
+  const timestamp = new Date().toISOString();
+  const method = req.method;
+  const url = req.originalUrl || req.url;
+  const queryParams = req.query;
+  const headers = req.headers;
+  const body = req.body;
+  // Log details to the console
+  console.log(`[${timestamp}] ${method} ${url}`);
+  console.log('Query Parameters:', queryParams);
+  console.log('Headers:', headers);
+  console.log('Body:', body);
+  // Save log to a file with timestamp
+  const logData = `[${timestamp}] ${method} ${url}\nQuery Parameters: ${JSON.stringify(queryParams)}\nHeaders: ${JSON.stringify(headers)}\nBody: ${JSON.stringify(body)}\n\n`;
+  const logFileName = `request_log_${timestamp.replace(/[^0-9]/g, '')}.txt`; // Remove non-numeric characters from timestamp
+  const logFilePath = path.join(__dirname, 'logs', logFileName);
+
+  fs.appendFile(logFilePath, logData, (err) => {
+    if (err) {
+      console.error('Error saving log to file:', err);
+    }
+  });
+
+  // Continue to the next middleware
+  next();
+}
+
+
+// Set up custom logging middleware
+app.use(customLogger);
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
+
+
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function(err, req, res) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- routes/users.js
+++ routes/users.js
@@ -5,5 +5,7 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
-
+router.post("/notbenit", function (req, res, next) {
+  res.send("hey")
+})
 module.exports = router;
