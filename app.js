b31a7b590bd4fac6d7fba76822c0c5accc37fbb9
Benit1 <110784868+Benit1@users.noreply.github.com>
2024-01-09 17:59:16 +0200

    middleware client to server done

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-console.log("benit")
+const fs = require('fs');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var app = express();
@@ -17,7 +17,37 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+const  customLogger = (req, res, next)=>  {
+  // Get request details
+  const timestamp = new Date().toISOString();
+  const method = req.method;
+  const url = req.originalUrl || req.url;
+  const queryParams = req.query;
+  const headers = req.headers;
+  const body = req.body;
+  // Log details to the console
+  console.log(`[${timestamp}] ${method} ${url}`);
+  console.log('Query Parameters:', queryParams);
+  console.log('Headers:', headers);
+  console.log('Body:', body);
+  // Save log to a file with timestamp
+  const logData = `[${timestamp}] ${method} ${url}\nQuery Parameters: ${JSON.stringify(queryParams)}\nHeaders: ${JSON.stringify(headers)}\nBody: ${JSON.stringify(body)}\n\n`;
+  const logFileName = `request_log_${timestamp.replace(/[^0-9]/g, '')}.txt`; // Remove non-numeric characters from timestamp
+  const logFilePath = path.join(__dirname, 'logs', logFileName);
 
+  fs.appendFile(logFilePath, logData, (err) => {
+    if (err) {
+      console.error('Error saving log to file:', err);
+    }
+  });
+
+  // Continue to the next middleware
+  next();
+}
+
+
+// Set up custom logging middleware
+app.use(customLogger);
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
