2d81426c08d4ae782c45a2aa187cf9574f0d51f0
amit kabalo <kabaloamit@gmail.com>
2024-01-16 18:59:41 +0200

    Created Message

---
+++ entities/Message.js
@@ -0,0 +1,9 @@
+class Message {
+    constructor(message, data, state) {
+        this.message = message;
+        this.data = data;
+        this.state = state;
+    }
+}
+
+module.exports = Message;
