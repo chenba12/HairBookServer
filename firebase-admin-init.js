b7115df89853c2da1b2c720eb10a1262fa5ff997
Chen Ben Ami <chen24201@gmail.com>
2024-01-16 15:22:27 +0200

    firebase-admin-init.js

--- firebase-admin-init.js
+++ firebase-admin-init.js
@@ -5,7 +5,6 @@ const {getFirestore} = require("firebase-admin/firestore");
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
-    //databaseURL: 'https://your-project-id.firebaseio.com' // Replace with your database URL
 });
 const db = getFirestore();
 module.exports = {admin, db};
