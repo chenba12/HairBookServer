f6694436a2e56cea54b38d05cd054df30de98e6d
orben22 <orbenami22@gmail.com>
2024-01-16 18:45:03 +0200

    entities fix

--- entities/Booking.js
+++ entities/Booking.js
@@ -1,7 +1,6 @@
 class BookingDTO {
-    constructor({date, time, _user_id, _barber_id, _barbershop_id}) {
+    constructor({date, _user_id, _barber_id, _barbershop_id}) {
         this.date = date
-        this.time = time
         this._user_id = _user_id
         this._barber_id = _barber_id
         this._barbershop_id = _barbershop_id
