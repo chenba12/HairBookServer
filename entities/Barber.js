a032b2dd47ca85120ada606e52f42fa688618664
orben22 <orbenami22@gmail.com>
2024-01-17 18:14:27 +0200

    log dic

--- entities/Barber.js
+++ entities/Barber.js
@@ -1,8 +1,9 @@
 class BarberDTO {
-    constructor({email, first_name, last_name, barber_shops}) {
+    constructor({email, first_name, last_name,years_of_experience, barber_shops}) {
         this.email=email
         this.first_name = first_name
         this.last_name = last_name
+        this.years_of_experience=years_of_experience
         this.barber_shops = barber_shops
     }
 }
